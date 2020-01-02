52eddb6b25a6306f4e33e113f9c93ea5a1b16a31
Afront <3943720+Afront@users.noreply.github.com>
2020-01-02 22:43:12 +0900

    Updated "local submodules"

--- January/week 1/login_screen_local/src/main.rs
+++ January/week 1/login_screen_local/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::io::Write;
 use std::process;
+use rpassword::read_password;
 
 fn abort() {
 	println!("AHHHHHHHHH");
@@ -18,12 +19,38 @@ fn signin() {
 }
 
 fn signup() {
-	println!("You want to sign up? Well, not today.");
-	abort();
+	loop {
+		print!("\x1B[2J");
+		let mut id = String::new();
+		let mut email = String::new();
+
+		print!("Please enter your ID: ");
+		io::stdout().flush().unwrap();
+		io::stdin().read_line(&mut id)
+				.expect("Failed to read line");
+
+		print!("Please enter your email: ");
+		io::stdout().flush().unwrap();
+		io::stdin().read_line(&mut email)
+				.expect("Failed to read line");
+
+		print!("Please enter your password: ");
+		io::stdout().flush().unwrap();
+		let password = read_password().unwrap();
+		
+		print!("Please enter your password again: ");
+		io::stdout().flush().unwrap();
+		if password == read_password().unwrap() {
+			break;
+		}
+	}
+	//println!("You want to sign up? Well, not today.");
 }
 
-fn main() {
-	loop {
+fn login_screen(){
+	let mut not_authenticated = true;
+	while not_authenticated {
+		print!("\x1B[2J");
 		print!("Hello! Would you like to (R)egister or (S)ign in? ");
 		io::stdout().flush().unwrap();
 		let mut input = String::new();
@@ -35,7 +62,15 @@ fn main() {
 				"SIGN UP" | "SIGNUP" | "REGISTER" | "R" => signup(),
 				"SIGN IN" | "SIGNIN" | "LOGIN" | "LOG IN" | "S" => signin(),
 				_  => continue,
-		};		
+		};
 	}
+}
 
+fn do_something(){
+
+}
+
+fn main() {
+	login_screen();
+	do_something();
 }
