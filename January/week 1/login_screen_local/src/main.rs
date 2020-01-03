4bfa3249593a7c77ad939115f6d3f47fc9c5c0b0
Afront <3943720+Afront@users.noreply.github.com>
2020-01-03 23:59:37 +0900

    Updated "local submodules"

--- January/week 1/login_screen_local/src/main.rs
+++ January/week 1/login_screen_local/src/main.rs
@@ -1,24 +1,81 @@
-use std::io;
+use std::{env, io};
 use std::io::Write;
 use std::process;
+use argonautica::Hasher;
+use reqwest::Response;
 use rpassword::read_password;
+use serde::{Deserialize, Serialize};
+//use serde_json::{Result};
 
-fn abort() {
+#[derive(Serialize, Deserialize)]
+struct User {
+	id: String,
+	email: String,
+	password: String,
+}
+
+#[derive(Debug)]
+enum LoginResult {
+	AlreadyQuited(bool),
+	AuthCode(String),
+	Error(String),
+	Helped(bool),
+	SignedUp(bool),
+}
+
+
+
+
+fn hash(password: String) -> String {
+	let mut hasher = Hasher::default();
+	let hash = hasher
+		.with_password(password)
+		.with_salt("this will not be the actual salt")
+		.with_secret_key("this will not be the secret key, just a placeholder")
+		.hash()
+		.unwrap();
+	println!("{}", &hash);
+	hash
+}
+
+fn abort() -> Result<LoginResult, LoginResult>{
 	println!("AHHHHHHHHH");
 	process::exit(0);
+	let x: Result<LoginResult, LoginResult> = Ok(LoginResult::AlreadyQuited(true));
+	x //since the compiler will complain...
 }
 
-fn help() {
+fn help() -> Result<LoginResult, LoginResult> {
 	println!("You need help? I don't think I'm the right person to ask. Try calling someone on your phone.");
 	abort();
+	let x: Result<LoginResult, LoginResult> = Ok(LoginResult::Helped(true));
+	x //since the compiler will complain...
 }
 
-fn signin() {
+fn signin() -> Result<LoginResult, LoginResult> {
 	println!("You want to sign in? Well, not today.");
 	abort();
+	let x: Result<LoginResult, LoginResult> = Ok(LoginResult::AuthCode(String::from("some_auth_code")));
+	x //since the compiler will complain...
 }
 
-fn signup() {
+async fn send_json(user_json: serde_json::Result<String>) -> reqwest::Result<Response> {
+	let client = reqwest::Client::new();
+	let server_url = env::var("SERVER_URL").expect("SERVER_URL must be set");
+
+	let res = client.post(&server_url)
+		.body("hi!")
+		.send()
+		.await?;
+
+	println!("{:?}", &user_json);
+	println!("{:?}", &res);
+
+	let x: reqwest::Result<Response> = Ok(res);
+	x
+}
+
+async fn signup() -> Result<LoginResult, LoginResult> {
 	loop {
 		print!("\x1B[2J");
 		let mut id = String::new();
@@ -41,13 +98,22 @@ fn signup() {
 		print!("Please enter your password again: ");
 		io::stdout().flush().unwrap();
 		if password == read_password().unwrap() {
+			let user = User {
+				id: id,
+				email: email,
+				password: hash(password)	
+			};
+			let user_json = serde_json::to_string(&user);
+			println!("{:?}", user_json);
+			send_json(user_json).await;
 			break;
 		}
 	}
-	//println!("You want to sign up? Well, not today.");
+	let x: Result<LoginResult, LoginResult> = Ok(LoginResult::SignedUp(true));
+	x
 }
 
-fn login_screen(){
+async fn login_screen() -> io::Result<()>{
 	let mut not_authenticated = true;
 	while not_authenticated {
 		print!("\x1B[2J");
@@ -59,18 +125,23 @@ fn login_screen(){
 		match input.trim().to_uppercase().as_str() {
 				"ABORT" | "EXIT" | "Q" | "QUIT" => abort(),
 				"HELP" | "H" => help(),
-				"SIGN UP" | "SIGNUP" | "REGISTER" | "R" => signup(),
+				"SIGN UP" | "SIGNUP" | "REGISTER" | "R" => signup().await,
 				"SIGN IN" | "SIGNIN" | "LOGIN" | "LOG IN" | "S" => signin(),
 				_  => continue,
 		};
+		if 1+1==2 { //will be an authentication code or something like that later on...
+			not_authenticated = false;
+		}
 	}
+	Ok(())
 }
 
+/**
 fn do_something(){
 
 }
+**/
 
-fn main() {
-	login_screen();
-	do_something();
+async fn main() -> io::Result<()> {
+	login_screen().await
 }
---
+++ January/week 1/login_screen_local/src/test.rs
@@ -0,0 +1,7 @@
+use std::io;
+
+
+async fn main() -> io::Result<()> {
+	let a: io::Result<()> = Ok(());
+	a
+}
