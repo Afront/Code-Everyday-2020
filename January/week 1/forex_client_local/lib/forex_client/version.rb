3fc109c2284cf30ef85ff8492a637619c8e045dd
Afront <3943720+Afront@users.noreply.github.com>
2020-01-06 00:13:29 +0900

    Deleted "local submodules"

--- January/week 1/forex_client_local/lib/forex_client/version.rb
+++
@@ -1,3 +0,0 @@
-module ForexClient
-  VERSION = "0.1.0"
-end
--- January/week 1/forex_client_local/test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "forex_client"
-
-require "minitest/autorun"
-#require "minitest/reporters"
-#Minitest::Reporters.use!
